b10741d76b6ab9f7f290720f8e62f2831087c782
CawkO <zaragosa667@gmail.com>
2020-10-07 19:36:43 +0300

    comit 3

--- index.php
+++ index.php
@@ -3,4 +3,24 @@
 $a = 1.10;
 $b = 'eeee';
 
-var_dump(isset ($a));
+// var_dump(isset ($a));
+// первый масив
+$arr = array(1, 'ewq', false);
+// второй масив
+$arr2 = [
+    'age'=>30,
+    'name'=>'Cawko',
+    'mail' => true,
+    'arr'=> [
+        'a' => 1,
+        'b' => '2'
+    ]
+    ];
+// var_dump($arr);
+// var_dump($arr2); 
+
+// обьект
+$obj = new stdClass;
+$obj -> a = 'i am a';
+
+var_dump($obj);
