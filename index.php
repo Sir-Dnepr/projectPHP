69ef7020cbdd690f68541fb9f7c1a9b0027aef34
CawkO <zaragosa667@gmail.com>
2020-10-08 18:47:41 +0300

    string

--- index.php
+++ index.php
@@ -1,26 +1,11 @@
 <?php
+$a = 'quest';
+$b = 'sky';
 
-$a = 1.10;
-$b = 'eeee';
-
-// var_dump(isset ($a));
-// первый масив
-$arr = array(1, 'ewq', false);
-// второй масив
-$arr2 = [
-    'age'=>30,
-    'name'=>'Cawko',
-    'mail' => true,
-    'arr'=> [
-        'a' => 1,
-        'b' => '2'
-    ]
-    ];
-// var_dump($arr);
-// var_dump($arr2); 
-
-// обьект
-$obj = new stdClass;
-$obj -> a = 'i am a';
-
-var_dump($obj);
+$final = <<< HEREDOC
+"my name is {$b}"
+HEREDOC;
+$c = `dir`;
+$d = $a . $b;
+var_dump($final);
+var_dump($c);
