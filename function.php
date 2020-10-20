c83f434de724a6d5edfc5898dbe0e64ab49779b1
CawkO <zaragosa667@gmail.com>
2020-10-20 11:30:13 +0300

    test $_GET

---
+++ function.php
@@ -0,0 +1,23 @@
+<?php
+
+
+function getArticles(){
+    return[
+        '1' =>[
+            'id'=> 1,
+            'title' => 'hello world',
+            'content' => ' hello all world! Some time'
+        ],
+        '2' =>[
+            'id'=> 2,
+            'title' => 'other article!',
+            'content' => ' some text about other!'
+        ],
+        '3' =>[
+            'id'=> 5,
+            'title' => 'one more article!',
+            'content' => ' some text about other one more!'
+        ],
+    ];
+
+}
--- index.php
+++ index.php
@@ -1,11 +1,23 @@
 <?php
-$a = 'quest';
-$b = 'sky';
 
-$final = <<< HEREDOC
-"my name is {$b}"
-HEREDOC;
-$c = `dir`;
-$d = $a . $b;
-var_dump($final);
-var_dump($c);
+include_once('function.php');
+
+$articles = getArticles();
+
+// print_r($articles);
+
+?>
+<div class = 'articles'>
+<? foreach ($articles as $id => $article):  ?>
+
+    <div class='article'>
+        <h3><?=$article['title']?></h3>
+        <a href="function.php?id=<?=$id?>">Read more</a>
+    </div>
+
+   
+ <?endforeach?>;
+
+
+</div>
+
